8996f8859e89bc03f7b4bc7914de1529a9415dc1
梁娅勤 <liangyaqin@bytedance.com>
2023-07-01 17:33:37 +0800

    temp: 测试

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:<node_version>
 
 COPY . .
 
+./build.sh
+
 EXPOSE 3000
 
 CMD ["./setup.sh"]
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -e # 遇到返回值非零退出
 set -x # 显示执行的命令
 
+git submodule update --init --recursive
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -11,5 +13,18 @@ nvm use lts/hydrogen
 
 echo 'node version is' && node -v
 
+npm i -g pnpm
+npm i -g yarn
+
+cd ./arco-design
+
+yarn install
+pnpm icon
+pnpm build:es
+pnpm build:css
+
 cd ../goodsman
-npm run start
+pnpm install
+pnpm build
+
+cd ../
